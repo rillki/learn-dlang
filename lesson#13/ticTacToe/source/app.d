0a966c53ad975fca65f08f70956db65b9a57e4bd
Ki Rill <rillk500@gmail.com>
2020-03-08 20:09:41 +0600

    Add files via upload

---
+++ lesson#13/ticTacToe/source/app.d
@@ -0,0 +1,123 @@
+import data;
+
+import raylib;
+
+import std.conv: to;
+
+void main() {
+	// init
+	InitWindow(WIDTH, HEIGHT, "Dlang Tic Tac Toe");
+	SetTargetFPS(30);
+	SetMouseScale(1.0, 1.0);
+
+	// load textures => x and o
+	Texture2D tcross = LoadTexture("res/cross.png");
+	Texture2D tnought = LoadTexture("res/nought.png");
+
+	// initialize squares using Square() constructor assinging the corresponding x and y position
+	Square[3][3] squares;
+	for(int i = 0; i < squares.length; i++) {
+		for(int j = 0; j < squares[i].length; j++) {
+			squares[i][j] = Square(Rectangle(i*SIZE, j*SIZE, SIZE, SIZE));
+		}
+	}
+
+	bool playersMove = true;	// tracking whose turn it is to make a move => x or o
+	bool draw = false; 			// if gameover and it is a draw
+	bool gameOver = false;		// if gameover and somebody has won => x or o
+
+	int empty_squares = 9;
+	while(!WindowShouldClose()) {
+		//	UPDATE => GAME LOGIC
+		Vector2 mousePos = GetMousePosition();
+
+		// checking rows, colomn, diagonals
+		if(squares[0][0].type != ' ' && squares[0][0].type == squares[0][1].type && squares[0][1].type == squares[0][2].type) {
+			gameOver = true;
+		}
+
+		if(squares[1][0].type != ' ' && squares[1][0].type == squares[1][1].type && squares[1][1].type == squares[1][2].type) {
+			gameOver = true;
+		}
+
+		if(squares[2][0].type != ' ' && squares[2][0].type == squares[2][1].type && squares[2][1].type == squares[2][2].type) {
+			gameOver = true;
+		}
+
+		if(squares[0][0].type != ' ' && squares[0][0].type == squares[1][0].type && squares[1][0].type == squares[2][0].type) {
+			gameOver = true;
+		}
+
+		if(squares[0][1].type != ' ' && squares[0][1].type == squares[1][1].type && squares[1][1].type == squares[2][1].type) {
+			gameOver = true;
+		}
+
+		if(squares[0][2].type != ' ' && squares[0][2].type == squares[1][2].type && squares[1][2].type == squares[2][2].type) {
+			gameOver = true;
+		}
+
+		if(squares[0][0].type != ' ' && squares[0][0].type == squares[1][1].type && squares[1][1].type == squares[2][2].type) {
+			gameOver = true;
+		}
+
+		if(squares[2][0].type != ' ' && squares[2][0].type == squares[1][1].type && squares[1][1].type == squares[0][2].type) {
+			gameOver = true;
+		}
+
+		if(empty_squares < 1 && !gameOver) {
+			draw = true;
+		}
+
+		// PROCESSING EVENTS 
+		if(IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) || IsMouseButtonPressed(MouseButton.MOUSE_RIGHT_BUTTON)) {
+			// getting i and j ID of squares
+			// since we convert x and y pos divided by SIZE to an integer, we get the exact number (i, j) of the square our mouse points to
+			// => 90/128 < 0 and if we convert it to an integer we get 0
+			int i = to!int(mousePos.x/SIZE);
+			int j = to!int(mousePos.y/SIZE);
+
+			// the player cannot override square type, therefore, if a square has been marked by x or o, it stays that way
+			if(squares[i][j].type == ' ') {
+				if(playersMove) {
+					squares[i][j].tex = tcross;
+					squares[i][j].type = 'x';
+				} else {
+					squares[i][j].tex = tnought;
+					squares[i][j].type = 'o';
+				}
+
+				empty_squares--;
+				playersMove = !playersMove;
+			}
+		}
+
+		// DRAW
+		BeginDrawing();
+		ClearBackground(Color(0, 179, 255, 255));
+		drawGrid();
+
+		// drawing squares
+		for(int i = 0; i < squares.length; i++) {
+			for(int j = 0; j < squares[i].length; j++) {
+				squares[i][j].draw();
+			}
+		}
+
+		if(draw) { 				// if the game is a draw
+			DrawRectangleRec(Rectangle(0, 0, WIDTH, HEIGHT), Color(0, 0, 0, 160));
+			DrawText("It's a draw!", WIDTH/16, HEIGHT*2/5, 60, WHITE);
+		} else if(gameOver) { 	// if somebody has won
+			DrawRectangleRec(Rectangle(0, 0, WIDTH, HEIGHT), Color(0, 0, 0, 160));
+			DrawText("You won!", WIDTH/8, HEIGHT*2/5, 70, WHITE);
+		}
+
+		EndDrawing();
+	}
+
+	// free memory
+	UnloadTexture(tcross);
+	UnloadTexture(tnought);
+
+	// quit
+	CloseWindow();
+}
---
+++ lesson#13/ticTacToe/source/data.d
@@ -0,0 +1,35 @@
+module data;
+
+import raylib;
+
+immutable int WIDTH = 384;
+immutable int HEIGHT = 384;
+immutable int SIZE = WIDTH/3;
+
+struct Square {
+	Rectangle rect;
+	Texture2D tex;
+
+	char type = ' ';
+
+	this(Rectangle r) {
+		rect = r;
+	}
+
+	void draw() {
+		// if the square has no type, return (skip the rest of the code)
+		if(type == ' ') {
+			return;
+		}
+
+		DrawTextureRec(tex, rect, Vector2(rect.x, rect.y), WHITE);
+	}
+}
+
+// drawing the Tic Tac Toe grid => 2 vertical and 2 horizontal lines
+void drawGrid() {
+	DrawLineEx(Vector2(WIDTH/3, 0), Vector2(WIDTH/3, HEIGHT), 3, WHITE);
+	DrawLineEx(Vector2(WIDTH*2/3, 0), Vector2(WIDTH*2/3, HEIGHT), 3, WHITE);
+	DrawLineEx(Vector2(0, HEIGHT/3), Vector2(WIDTH, HEIGHT/3), 3, WHITE);
+	DrawLineEx(Vector2(0, HEIGHT*2/3), Vector2(WIDTH, HEIGHT*2/3), 3, WHITE);
+}
