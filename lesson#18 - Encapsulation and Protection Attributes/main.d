a6512db88e23bef463a3b9e1529230e4e31a93ae
Ki Rill <rillk500@gmail.com>
2020-07-10 12:46:30 +0600

    Update main.d

--- lesson#18 - Encapsulation and Protection Attributes/main.d
+++ lesson#18 - Encapsulation and Protection Attributes/main.d
@@ -10,5 +10,5 @@ void main() {
 	// Mary's members can be accessed through get... functions, but can't be modified
 	Person2 Mary = Person2("Mary", 21);
 	writeln("Person: ", Mary.name, ", ", Mary.age);				// ERROR! => no such property: name, age (because it's private)
-	writeln("Person: ", Mary.getName(), ", ", Mary.getAge());				// works! => a copy/value is returned
+	writeln("Person: ", Mary.getName(), ", ", Mary.getAge());		// works! => a copy/value is returned
 }
